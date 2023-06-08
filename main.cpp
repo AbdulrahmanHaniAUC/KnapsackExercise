e476b1af8eca0e2fa3cd34f0f6f774afaf3921e4
“Abdulrahman <Abdulrahmanhani@aucegypt.edu>
2023-06-08 17:00:01 +0300

    main file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "knapsack"
+
+using namespace std;
+
+int main () {
+int val[] = {60, 100, 120};
+int wt[] = {10, 20, 30};
+
+int w = 50;
+int n = sizeof(val) / sizeof(val[0])
+
+cout << knapSack(W, wt, val, n);
+
+return 0;
+}
